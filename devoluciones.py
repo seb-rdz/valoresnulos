5944cb53c098061b5049d0a1cda3a8fb66b0ac43
seb-rdz <A01709461@tec.mx>
2024-04-17 11:35:30 -0600

    limpie "devoluciones.xlsx" y generé el archivo "devoluciones_limpio.csv"

--- devoluciones.py
+++ devoluciones.py
@@ -1,3 +1,31 @@
 import pandas as pd
 df = pd.read_excel('devoluciones.xlsx')
-print(df.head())
+#print(df.head())
+#print(df.isnull().sum())
+
+# Aquí reemplazo los nulos en la columna "CVE_VEND" con un espacio en
+# blanco " " ya que no puedo crear una clave de vendedor o realizar 
+# una imputación que podría llevar a futuros errores. 
+df['CVE_VEND']=df['CVE_VEND'].fillna(' ')
+#print(df.isnull().sum())
+
+# Aquí reemplazo los nulos en la columna "CVE_PED" con un espacio en 
+# blanco " " ya que no puedo realizar una imputación con claves de pedidos
+# que podrían ser erróneas
+df['CVE_PEDI']=df['CVE_PEDI'].fillna(' ')
+#print(df.isnull().sum())
+
+# Aquí reemplazo los nulos en la columna "FECHA_CANCELA" con un espacio en
+# blanco " " ya que no puedo realizar una imputación de fechas dado que 
+# podrían imputarse fechas erróneas 
+df['FECHA_CANCELA'] = df['FECHA_CANCELA'].fillna(' ')
+#print(df.isnull().sum())
+
+# Aquí reemplazo los nulos en la columna "DOC_ANT" con un espacio en blanco 
+# " " ya que realizar una imputación para esta columna podría causar más 
+# errores dado que no puedo crear una clave de documento imaginaria
+df['DOC_ANT'] = df['DOC_ANT'].fillna(' ')
+print(df.isnull().sum())
+
+# Convertir DataFrame a CSV 
+df.to_csv('devoluciones_limpio.csv')
